ba538f1ed3c7ca710a2c5d181ea26805ffd24fb4
nkhakwan <post_khan@yahoo.com>
2020-06-05 15:16:40 -0700

    some cleaning in a controller file

--- Sweet/Controllers/FlavorsController.cs
+++ Sweet/Controllers/FlavorsController.cs
@@ -1,10 +1,3 @@
-// using Microsoft.AspNetCore.Mvc;
-// using Sweet.Models;
-// using System.Collections.Generic;
-// using System.Linq;
-// using Microsoft.EntityFrameworkCore;
-// using System;
-
 using Microsoft.AspNetCore.Mvc;
 using Sweet.Models;
 using System.Collections.Generic;
